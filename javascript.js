0c62aecb97ddf9c9ed69a42d0876972c0dc7b649
Ivan Oliveira <79813537+IvanIndaia@users.noreply.github.com>
2021-11-28 11:11:16 -0300

    Almost

--- javascript.js
+++ javascript.js
@@ -1,26 +1,17 @@
-function apostar(){
+function apostar () {
     var fla = document.getElementById("placarF").value
     var pal = document.getElementById("placarP").value
     var apostador = document.getElementById("nomeApostador").value
 
-       
-    var r = confirm("Confirma a aposta de: \n PAL "+pal+" X "+fla+" FLA")
-
-    if (r == true) {
-        if (fla == pal){
-            alert(apostador + " você apostou no empate!!!")
-            var pPal = prompt("Quantos penalts o Palmeiras vai acertar?");
-            var pFla = prompt("Quantos penalts o Flamengo vai acertar?");
-        }
+    if (fla == pal) {
+        /*setstyle '.\style.css' = '#penalte', visibility=hidden;*/
+        document.getElementById("penaltP").value = prompt("Quantos Penalts o Palmeiras vai acertar? ")
+        document.getElementById("penaltF").value = prompt("Quantos Penalts o Flamengo vai acertar? ")
     }else {
-        
+
     }
-processar(pal, fla,pPal,pFla)
-alert("APOSTA FEITA, PARA VALIDAR SÓ FAZER O PIX")
 }
-function processar(p, f, pp, pf) {
-    alert()
-}
+
 
 
 
